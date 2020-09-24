d4ae4b80028c1bc91bd9c13e03c7f3832d4f2a53
Ken Liu <kliu032@gmail.com>
2020-09-24 02:21:06 +0000

    No Comments

---
+++ Assets/Scripts/Building Parts/Building.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building
+{
+    Vector2Int size;
+	Wing[] wings;
+
+	public Vector2Int size {get {return size;}}
+	public Wing[] Wings {get{return wings;}}
+
+	public Buildings(int sizeX, int sizeY, Wings[] wings){
+		size = new Vector2int(sizeX, sizeY);
+		this.wings = wings;
+	}
+
+	public override string ToString()
+	{
+		string bldg = "Building:(" + size.ToString() + "; " + wings.Length + ")\n";
+		foreach (Wing w in wings){
+			bldg += "\t + w.ToString() + \n";
+		}
+		return bldg;
+	}
+}
